a12296fec2e139a10913fd8a4325ef55f0865bb1
Lucas Maume <lmaume@k0r3p10.42mulhouse.fr>
2025-01-24 18:21:19 +0100

    correction de l'appel d'init_struct qui demandais filename

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -1,10 +1,10 @@
 #include "parsing.h"
 
-int	parse_struct(t_map *data)
+int	parse_struct(t_map *data, char *filename)
 {
 	t_map	data_map;
 
-	if (init_struct(&data_map) == 1)
+	if (init_struct(&data_map, filename) == 1)
 		return (1);
 	return (0);
 }
